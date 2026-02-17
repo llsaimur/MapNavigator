190209d2f8efbe274ea5b2af81bab3b102dce085
Mohammad Saimur Rashid <rashidsaimur@icloud.com>
2026-02-17 15:29:54 -0500

    Initial Commit

---
+++ MapNavigator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapNavigator
+//
+//  Created by Saimur Rashid on 2/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapNavigator/MapNavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapNavigatorApp.swift
+//  MapNavigator
+//
+//  Created by Saimur Rashid on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct MapNavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
